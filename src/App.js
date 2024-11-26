c0329580a1854a500ac8ee96ff96d2f2bc670e48
parassareen4 <kyakrnahaibro@gmail.com>
2024-11-26 09:44:50 -0500

    cool

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
+  return (  
+   <>
+      <Header />
+   </>
   );
 }
-
+ 
 export default App;
