02d9092591a9eb86a61b0f4c4c356d09ec4e9b4b
parassareen4 <kyakrnahaibro@gmail.com>
2024-11-26 10:55:46 -0500

    cool

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <nav>
+        <NavContent />
+    </nav>
   )
 }
 
-export default Header
+const NavContent = () => {
+    return (
+        <>
+            <h2>Paras.</h2>
+            <div>
+                <a href='#home'>Home</a>
+                <a href='#words'>Word</a>
+                <a href='#timeline'>Expiriences</a>
+                <a href='#services'>Services</a>
+                <a href='#testimonials'>Testimonials</a>
+                <a href='#contact'>Contact</a>
+            </div>
+            <a href="mailto:kyakrnahaibro@gmail.com">
+                <button>Email</button>
+            </a>
+        </>
+    )
+}
+export default Header 
