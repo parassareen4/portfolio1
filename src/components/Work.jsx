964cf25d87769d4594b751cfd903a0b7ee1f7a29
parassareen4 <kyakrnahaibro@gmail.com>
2024-11-27 14:24:43 -0500

    work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import data from '../assets/data.json'
 
 function Work() {
   return (
-    <div>Work</div>
+    <div id="work">
+        <h2>Work</h2>
+        <section>
+            <article>
+                <Carousel>
+
+                </Carousel>
+            </article>
+        </section>
+    </div>
   )
 }
 
