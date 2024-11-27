26b6b64cb8e905f572546daee109292bfcbc1424
parassareen4 <kyakrnahaibro@gmail.com>
2024-11-27 18:51:52 -0500

    work

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -21,8 +21,11 @@ function Timeline() {
 const TimelineItem = ({heading,text,input,index}) => {
     return (
         <div className={`timelineItem ${index % 2 === 0 ? 'leftTimeline' : 'rightTimeline'}`}>
+            
+            <div>
             <h2>{heading}</h2>
             <p>{text}</p>
+            </div>
             
         </div>
     )
