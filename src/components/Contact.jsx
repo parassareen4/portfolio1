a62837a4efa2a4c9602d3a77249172a572e63230
parassareen4 <kyakrnahaibro@gmail.com>
2024-11-28 15:06:51 -0500

    services

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import vg from '../assets/hello.png'
 function Contact() {
+
+
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [message, setMessage] = useState('')
+
   return (
     <div id='contact'>
         
         <section>
             <form action="https://formspree.io/f/xmolqznw" method="POST">
                 <h2>Contact Me</h2>
-                <input type='text' placeholder="Your Name" required/>
-                <input type='email' placeholder="Your Email" required/>
-                <input type='text' placeholder="Your Message" required/>
+                <input type='text' value={name} onChange={(e)=>setName(e.target.value)} placeholder="Your Name" required/>
+                <input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Your Email" required/>
+                <input type='text' value={message} onChange={(e)=>setMessage(e.target.value)} placeholder="Your Message" required/>
                 <button type="submit">Send</button>      
             </form>
         </section>
